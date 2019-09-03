b059cea8f91a46381373c929c2fa1c039fd0e45e
Ivan Ivchenko <waltereevan@gmail.com>
2019-09-03 16:56:45 +0600

    Fix list convertation CNX-77

--- lib/ex_wubook/formatted_float.ex
+++ lib/ex_wubook/formatted_float.ex
@@ -1,6 +1,5 @@
 defmodule ExWubook.FormattedFloat do
 
-  def convert(float, exponent) do
-    XMLRPC.FormattedFloat.new({float, "~.#{exponent}f"})
-  end
+  def convert(list, exponent) when is_list(list), do:  Enum.map(list, &convert(&1, exponent))
+  def convert(float, exponent), do: XMLRPC.FormattedFloat.new({float, "~.#{exponent}f"})
 end
